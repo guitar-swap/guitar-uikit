89a713c72f0697d2eac64f163562b70166f6968d
Literally Collective <bluedragon.cstar@outlook.com>
2022-04-02 01:56:39 -0300

    Update index.cjs.js

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -4124,7 +4124,7 @@ var socials = [
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/Guitar_swap",
+        href: "https://twitter.com/Guitarswap_dex",
     },
 ];
 var MENU_HEIGHT = 64;
